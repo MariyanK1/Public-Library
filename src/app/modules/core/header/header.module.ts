44253fc0aaeaa4b9df9df01d923cca493a83d46c
Mariyan Maksimov <mariyan.pickup@gmail.com>
2022-05-27 10:42:29 +0300

    add material ui header

--- src/app/modules/core/header/header.module.ts
+++ src/app/modules/core/header/header.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './component/header.component';
 import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [HeaderComponent],
   exports: [HeaderComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatToolbarModule, MatButtonModule],
 })
 export class HeaderModule {}
